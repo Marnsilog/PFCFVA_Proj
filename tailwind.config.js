3d8fe969587d6e54861fc0e1f92439707f8019e6
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-04-04 16:43:52 +0800

    asdda

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,23 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-      maxHeight: {
-        'maxtest': '250px',
-      },
+
       minHeight: {
         'mintest': '35rem',
       },
       height: {
-        'mars': '60%', // You can also use fixed values like pixels
+        'mars': '60%', 
         '37':'150px',
         '78':'300px',
       },
       padding: {
-        'custom': '650px 700px 70px', // Custom padding value
+        'custom': '650px 700px 70px',
       },
       left:{
-        'test':'200%',
+        '97':'28rem',
+      },
+      top:{
+        '41':'110.313rem',
       },
     },
   },

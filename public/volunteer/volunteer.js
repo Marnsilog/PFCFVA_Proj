056a60c1f2876bc1970f57e0f2bf03b10686d624
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-04-04 19:03:59 +0800

    sad

---
+++ public/volunteer/volunteer.js
@@ -0,0 +1,8 @@
+function myImage() {
+    var form = document.getElementById("profileDetail");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}

115af367e603362f15ed75dce1e92eb18f95f2af
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-04-05 19:51:39 +0800

    asd

--- public/volunteer/volunteer.js
+++ public/volunteer/volunteer.js
@@ -2,7 +2,19 @@ function myImage() {
     var form = document.getElementById("profileDetail");
     if (form.style.display === "none") {
         form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
+    } 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("viewRankings").addEventListener("click", function() {
+        document.getElementById("profileStat").style.display = "none";
+        document.getElementById("rankingStat").style.display = "block";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("viewProfile").addEventListener("click", function() {
+        document.getElementById("rankingStat").style.display = "none";
+        document.getElementById("profileStat").style.display = "block";
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,40 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
-
       minHeight: {
         'mintest': '35rem',
       },
       height: {
         'mars': '60%', 
-        '37':'150px',
-        '78':'300px',
+        '37': '150px',
+        '78': '300px',
       },
       padding: {
         'custom': '650px 700px 70px',
       },
-      left:{
-        '97':'28rem',
+      inset: {
+        '97': '28rem',
+        '41': '110.313rem',
       },
-      top:{
-        '41':'110.313rem',
-      },
-      safelist:[
-        'bg-black',
-        'text-5xl',
-        'lg:text-4xl',
-      ]
+    },
+  },
+  safelist: {
+    standard: [
+      'bg-black',
+      'text-5xl',
+      'lg:text-4xl',
+    ],
+    deep: [
+      // Additional deep selectors can go here if needed
+    ],
+    greedy: [
+      // Additional greedy selectors can go here if needed
+    ],
+  },
+  variants: {
+    extend: {
+      padding: ['pt'], // This extends the padding utility with the pt variant
     },
   },
   plugins: [],
-}
+};

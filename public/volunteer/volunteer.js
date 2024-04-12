f52455eccea66c84bf1b54e7244182c77f24af03
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-04-13 02:50:01 +0800

    sda

--- public/volunteer/volunteer.js
+++ public/volunteer/volunteer.js
@@ -63,37 +63,71 @@ function animateProgressBar(targetWidth) {
           profileForm.style.display = 'none';
       }
   }
-  
+   
 
+  function showDashboard(){
+    document.getElementById('frmDashboard').style.display = 'block';
+    document.getElementById('frmContactus').style.display = 'none';
+    document.getElementById('frmHtvolunteer').style.display = 'none';
+    document.getElementById('frmInventory').style.display = 'none';
+    document.getElementById('frmMainProfile').style.display = 'none';
+    document.getElementById('Setting').style.display = 'none';
+    
+  }
+  function showContactus(){
+    document.getElementById('frmDashboard').style.display = 'block';
+    document.getElementById('frmContactus').style.display = 'block';
+    document.getElementById('frmHtvolunteer').style.display = 'none';
+    document.getElementById('frmInventory').style.display = 'none';
+    document.getElementById('frmMainProfile').style.display = 'none';
+    document.getElementById('Setting').style.display = 'none';
+    
+  }
+  function showHwVolunteer(){
+    document.getElementById('frmHtvolunteer').style.display = 'block';
+    document.getElementById('frmContactus').style.display = 'none';
+    document.getElementById('frmInventory').style.display = 'none';
+    document.getElementById('frmDashboard').style.display = 'none';
+    document.getElementById('frmMainProfile').style.display = 'none';
+    document.getElementById('Setting').style.display = 'none';
+    
+  }
   function myProfile(){
     document.getElementById('frmMainProfile').style.display = 'block';
+    document.getElementById('frmContactus').style.display = 'none';
+    document.getElementById('frmHtvolunteer').style.display = 'none';
+    document.getElementById('frmDashboard').style.display = 'none';
     document.getElementById('frmInventory').style.display = 'none';
     document.getElementById('Setting').style.display = 'none';
     
   }
+   
     function showInventory(){
       document.getElementById('frmInventory').style.display = 'block';
+      document.getElementById('frmContactus').style.display = 'none';
+      document.getElementById('frmHtvolunteer').style.display = 'none';
+      document.getElementById('frmDashboard').style.display = 'none';
       document.getElementById('frmMainProfile').style.display = 'none';
       document.getElementById('Setting').style.display = 'none';
       
     }
     //Changable form
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const logoutLink = document.getElementById('logoutLink');
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutLink = document.getElementById('logoutLink');
 
-//     if (logoutLink) {
-//         logoutLink.addEventListener('click', function(event) {
-//             event.preventDefault();
+    if (logoutLink) {
+        logoutLink.addEventListener('click', function(event) {
+            event.preventDefault();
 
-//             const confirmLogout = confirm("Are you sure you want to log out?");
+            const confirmLogout = confirm("Are you sure you want to log out?");
 
-//             if (confirmLogout) {
-//                 window.location.href = '/public/dashboard.html';
-//             }
-//         });
-//     }
-// });
+            if (confirmLogout) {
+                window.location.href = '/public/dashboard.html';
+            }
+        });
+    }
+});
 
 
 

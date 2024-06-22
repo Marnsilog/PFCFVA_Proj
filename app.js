426769c278661faefb692ee13b6f6edb28d6928e
Jamex <ar.james.contact@gmail.com>
2024-06-22 09:31:46 +0800

    cc

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-
+//add rfid to database
 //register route (test-hash)
 app.post('/register', (req, res) => {
     const {

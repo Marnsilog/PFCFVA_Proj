2dbc8835993d685c2bff44df00cd1f52222a7ac1
Jamex <ar.james.contact@gmail.com>
2024-07-16 10:45:34 +0800

    obj 1 God Mode

--- public/module_attendance.js
+++ public/module_attendance.js
@@ -40,6 +40,8 @@ function handleRFIDScan(rfid) {
     document.getElementById('rfidText').textContent = rfid;
     console.log('Handling RFID scan:', rfid);
 
+    changeBackgroundColor(); //hehe
+
     fetch('/recordTimeIn', {
         method: 'POST',
         headers: {
@@ -165,6 +167,24 @@ function fetchRecentAttendance() {
 }
 
 
+
+//colors
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+    const divs = document.querySelectorAll('div');
+    divs.forEach(div => {
+        div.style.backgroundColor = getRandomColor();
+    });
+}
 
 
 // //working for retrieving profile

d2c427d2f6e7f9c196fe76b24ffd8017ee90b77c
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-09-26 18:10:46 +0800

    dawdaw

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const fs = require('fs');
 const randomBytesAsync = promisify(crypto.randomBytes);
 
 // Create connections
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pfcfva'
 });
 
 // Connect
